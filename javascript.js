8e14490787b0ccaa152a27e8e423fd920e300e6a
Dario <jlsc.dario@gmail.com>
2023-05-16 09:15:44 +0200

    Correcion de errores

--- javascript.js
+++ javascript.js
@@ -5,4 +5,14 @@ function myFunction() {
     } else {
       x.className = "topnav";
     }
-  }
+}
+
+const fulImgBox =document.getElementById("fulImgBox");
+fulImg=document.getElementById("fulImg");
+function closeImg(){
+  fulImgBox.style.display="none"
+}
+function openFulImg(reference){
+  fulImgBox.style.display="flex";
+  fulImg.src=reference;
+}
